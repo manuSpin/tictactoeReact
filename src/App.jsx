28da3a4f2cbe096142707f0d097b0e4224054985
Manuel Fernández <manuel.ferca.91@gmail.com>
2025-05-08 09:17:24 +0200

    Gurdar la partida en el localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ import { Square } from "./components/Square";
 import { WinnerModal } from "./components/WinnerModal";
 import { TURNS } from "./constants";
 import { checkWinner } from "./logic/board";
+import { resetGameStorage, saveGameToStorage } from "./logic/storage";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn');
+    return turnFromStorage ? turnFromStorage : TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
   // null no hay ganador y false significa que hay un empate
 
@@ -30,6 +37,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    // Guardar la partida
+    saveGameToStorage({ board: newBoard, turn: newTurn })
+
     // Revisar si hay un ganador
     const newWinner = checkWinner(newBoard);
     console.log(newWinner);
@@ -48,6 +58,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    // Reiniciar el localStorage
+    resetGameStorage();
   }
 
 
